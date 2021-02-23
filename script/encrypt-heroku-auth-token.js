32a419f62a014872e879ad28b04854e83a88d38f
PigsGoMoo <azianknowitall@gmail.com>
2021-02-23 14:39:13 -0500

    Travis changes

--- script/encrypt-heroku-auth-token.js
+++ script/encrypt-heroku-auth-token.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-const { spawn } = require("child_process");
-const fs = require("fs");
+const { spawn } = require('child_process');
+const fs = require('fs');
 
-const axios = require("axios");
-const GitUrlParse = require("git-url-parse");
-const simpleGit = require("simple-git")();
-const YAML = require("yaml");
+const axios = require('axios');
+const GitUrlParse = require('git-url-parse');
+const simpleGit = require('simple-git')();
+const YAML = require('yaml');
 
 /* Specific message contents stored as constants */
 
-const keyComments = require("./keyComments.json");
+const keyComments = require('./keyComments.json');
 
 const idempotenceMessage = `It appears that your token has been encrypted.
 To run this script again, delete the \`before_deploy\` and \`deploy\` keys
@@ -20,8 +20,8 @@ const successMessage = `Complete! Run \`git diff .travis.yml\` to check.`;
 
 /* Clean up system state changes. */
 const clean = () => {
-  const externalFiles = [".tmp.key.pem", ".tmp.token.txt", ".tmp.token.enc"];
-  externalFiles.forEach(file => {
+  const externalFiles = ['.tmp.key.pem', '.tmp.token.txt', '.tmp.token.enc'];
+  externalFiles.forEach((file) => {
     if (fs.existsSync(file)) fs.unlinkSync(file);
   });
 };
@@ -29,7 +29,7 @@ const clean = () => {
 /* Get a specific git remote URL. */
 const getRemoteURL = (name, remotes) => {
   try {
-    return remotes.filter(remote => remote.name === name)[0].refs.fetch;
+    return remotes.filter((remote) => remote.name === name)[0].refs.fetch;
   } catch (err) {
     console.log(
       `It appears that the remote ${name} does not exist.`,
@@ -42,24 +42,38 @@ const getRemoteURL = (name, remotes) => {
 /* Run a command and return its stdout. */
 const getOutputFromCommand = async (command, args) => {
   const response = await new Promise((resolve, reject) => {
+    console.log(`Running command: ${command}\nWith args: ${args}`);
     const process = spawn(command, args);
-
+    console.log(`Command ran. ${JSON.stringify(process.stdout)}`);
     const stdout = [];
     const stderr = [];
-
-    process.stdout.on("data", data => {
+    console.log(`stdout call`);
+    process.stdout.on('data', (data) => {
       stdout.push(data);
     });
-
-    process.stderr.on("data", data => {
+    console.log(`stdout call done. `);
+    process.stderr.on('data', (data) => {
       stderr.push(data);
     });
 
-    process.on("close", code => {
-      if (code) throw new Error(reject(stderr));
-      resolve(stdout);
+    console.log(`stderr call done`);
+    console.log(`Error?`);
+
+    process.on('error', (err) => {
+      console.log(`error: `, err);
+      throw new Error(reject(stderr));
     });
+
+    console.log(`No error...on exit...`);
+    process.on('exit', (code) => {
+      console.log(`Child exited with code: ${code}`);
+      //   if (code) throw new Error(reject(stderr));
+      //   resolve(stdout);
+    });
+    console.log(`Resolving...`);
+    resolve(stdout);
   });
+  console.log(`Exiting getOutputFromCommand`);
   return response;
 };
 
@@ -69,56 +83,57 @@ const getNamesFromGit = () =>
     simpleGit.getRemotes(true, (err, res) => {
       if (err) throw new Error(reject(err));
       resolve({
-        fullName: GitUrlParse(getRemoteURL("origin", res)).full_name,
-        appName: GitUrlParse(getRemoteURL("heroku", res)).name
+        fullName: GitUrlParse(getRemoteURL('origin', res)).full_name,
+        appName: GitUrlParse(getRemoteURL('heroku', res)).name,
       });
     })
   );
 
 /* Use the openssl command to encrypt an authentication token. */
 const encryptHerokuToken = async () => {
-  await getOutputFromCommand("openssl", [
-    "rsautl",
-    "-encrypt",
-    "-pubin",
-    "-inkey",
-    ".tmp.key.pem",
-    "-in",
-    ".tmp.token.txt",
-    "-out",
-    ".tmp.token.enc"
+  await getOutputFromCommand('openssl', [
+    // 'openssl',
+    'rsautl',
+    '-encrypt',
+    '-pubin',
+    '-inkey',
+    '.tmp.key.pem',
+    '-in',
+    '.tmp.token.txt',
+    '-out',
+    '.tmp.token.enc',
   ]);
 };
 
 /* Write the encrypted key, and other values, to the .travis.yml file. */
 const updateTravisYAML = (app, key) => {
-  const travis = fs.readFileSync(".travis.yml", "utf8");
+  const travis = fs.readFileSync('.travis.yml', 'utf8');
   const doc = YAML.parseDocument(travis);
-  if (doc.has("before_deploy")) {
+  if (doc.has('before_deploy')) {
     return console.log(idempotenceMessage);
   }
-  doc.set("before_deploy", ["rm -rf node_modules"]);
+  doc.set('before_deploy', ['rm -rf node_modules']);
   doc.set(
-    "deploy",
+    'deploy',
     YAML.createNode({
       skip_cleanup: true, //eslint-disable-line
-      provider: "heroku",
+      provider: 'heroku',
       app: app,
-      api_key: { secure: key } //eslint-disable-line
+      api_key: { secure: key }, //eslint-disable-line
     })
   );
   doc.contents.items
-    .filter(item => item.key in keyComments)
-    .forEach(item => {
+    .filter((item) => item.key in keyComments)
+    .forEach((item) => {
       item.comment = keyComments[item.key];
-      if (item.key === "deploy") {
-        item.value.items.forEach(item_ => {
+      if (item.key === 'deploy') {
+        item.value.items.forEach((item_) => {
           item_.commentBefore = keyComments[item_.key];
         });
       }
     });
-  doc.comment = "";
-  fs.writeFileSync(".travis.yml", doc.toString());
+  doc.comment = '';
+  fs.writeFileSync('.travis.yml', doc.toString());
   return true;
 };
 
@@ -127,28 +142,32 @@ const main = async () => {
   const { fullName, appName } = await getNamesFromGit();
 
   /* Get Heroku authentication token from the Heroku CLI. */
-  const herokuTokenOut = await getOutputFromCommand("heroku", ["auth:token"]);
-  const herokuTokenStr = herokuTokenOut.toString("utf-8");
+  const herokuTokenOut = await getOutputFromCommand('cmd', [
+    // 'heroku',
+    'heroku auth:token',
+  ]);
+  const herokuTokenStr = herokuTokenOut.toString('utf-8');
   const herokuToken = herokuTokenStr.slice(0, herokuTokenStr.length - 1);
-  if (verbose) console.log("Received Heroku token", herokuToken.toString());
+  if (verbose) console.log('Received Heroku token', herokuToken.toString());
 
   /* Download the repo's public key supplied by Travis. */
   const travisURL = `https://api.travis-ci.org/repos/${fullName}/key`;
   const travisResponse = await axios.get(travisURL);
   const key = travisResponse.data.key;
-  const keyBuffer = Buffer.from(key, "utf-8");
-  if (verbose) console.log("Received Travis pubkey:\n", keyBuffer.toString());
+  const keyBuffer = Buffer.from(key, 'utf-8');
+  if (verbose) console.log('Received Travis pubkey:\n', keyBuffer.toString());
 
   /* Write files for use with openssl */
-  fs.writeFileSync(".tmp.key.pem", key);
-  fs.writeFileSync(".tmp.token.txt", herokuToken);
+  fs.writeFileSync('.tmp.key.pem', key);
+  fs.writeFileSync('.tmp.token.txt', herokuToken);
 
   /* Encrypt the Heroku token and save it in the .tmp.token.enc file. */
   await encryptHerokuToken();
 
   /* Encode the encrypted data in base64. */
-  const keyBase64 = fs.readFileSync(".tmp.token.enc").toString("base64");
-  if (verbose) console.log("Encrypted key base 64 encoded:", keyBase64);
+  console.log(`Reading .tmp.token.enc`);
+  const keyBase64 = fs.readFileSync('.tmp.token.enc').toString('base64');
+  if (verbose) console.log('Encrypted key base 64 encoded:', keyBase64);
 
   /* Delete temporary files. */
   clean();
@@ -158,15 +177,15 @@ const main = async () => {
   if (update) console.log(successMessage);
 
   /* Clean up in the case of unspecified errors. */
-  process.on("uncaughtException", () => {
+  process.on('uncaughtException', () => {
     clean();
-    if (verbose) console.log("Cleaned up on error!");
+    if (verbose) console.log('Cleaned up on error!');
     process.exit(1);
   });
 
-  process.on("unhandledRejection", () => {
+  process.on('unhandledRejection', () => {
     clean();
-    if (verbose) console.log("Cleaned up on error!");
+    if (verbose) console.log('Cleaned up on error!');
     process.exit(1);
   });
 };
